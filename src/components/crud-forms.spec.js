ce387f3897e27136396631181c9311253c622bf5
Elias Cole <elias.cole@comcast.com>
2017-06-04 18:26:05 -0700

    fixed class properties mocha issue

--- src/components/crud-forms.spec.js
+++ src/components/crud-forms.spec.js
@@ -1,7 +1,7 @@
 import { expect } from 'chai'
 import React from 'react'
 import { shallow, mount } from 'enzyme'
-// import { BeerListContainer, InputArea, BeerList } from './components'
+import { CreateEntryForm, UpdateEntryForm } from './crud-forms'
 import { spy } from 'sinon'
 
 // import { clone } from '../src/toolbox/clone'
